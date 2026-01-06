5af187d4ba563a810d50ce28d61580f0fe024138
RNOL14 <rnolmochahari756@gmail.com>
2026-01-06 22:22:50 +0530

    basics of xaml and maui

---
+++ Maui/MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void OnShowNameClicked(object sender, EventArgs e)
+{
+    string name = nameEntry.Text;
+    DisplayAlert("Name", $"Hello {name}", "OK");
+}
+}
---
+++ Maui/MauiApp3/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        string username = usernameEntry.Text;
+        string password = passwordEntry.Text;
+
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            await DisplayAlert(
+                "Error",
+                "Please enter username and password",
+                "OK"
+            );
+        }
+        else
+        {
+            await DisplayAlert(
+                "Success",
+                $"Welcome {username}",
+                "OK"
+            );
+        }
+    }
+
+    private void OnClearClicked(object sender, EventArgs e)
+    {
+        usernameEntry.Text = string.Empty;
+        passwordEntry.Text = string.Empty;
+    }
+}
