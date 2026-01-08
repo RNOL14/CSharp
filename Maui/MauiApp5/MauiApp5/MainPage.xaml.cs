3e7c5c933daf25210c05bbad31b53ccfb307b053
RNOL14 <rnolmochahari756@gmail.com>
2026-01-08 14:46:23 +0530

    User preferences

---
+++ Maui/MauiApp5/MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+	private async void OnSaveClicked(object sender, EventArgs e)
+	{
+		string name = NameEntry.Text ?? "User";
+		bool notifications = NotifySwitch.IsToggled;
+
+		string gender =
+				MaleRadio.IsChecked ? "Male" :
+				FemaleRadio.IsChecked ? "Female" :
+				OtherRadio.IsChecked ? "Other" : "Not selected";
+
+		string language = LanguagePicker.SelectedItem?.ToString() ?? "Not selected";
+
+		await DisplayAlert(
+				"Saved",
+				$"Name: {name}\nGender: {gender}\nLanguage: {language}\nNotifications: {notifications}",
+				"OK"
+		);
+	}
+}
